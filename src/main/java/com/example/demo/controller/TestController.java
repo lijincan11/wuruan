5cd16ca4fac529465476558b8dc4c1dbd455bcc5
Xiong Zhao <13657211670@sina.cn>
2018-11-18 15:37:42 +0800

    我新增了  两个接口

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,24 @@ public class TestController {
 		System.out.println("hello"+name);
 		
 		return "hello "+name+"!!!!~~~";
+	}
+	
+	
+	
+	@RequestMapping("/play/{name}")
+	public String play(@PathVariable("name") String name){
+		
+		System.out.println("hello"+name);
+		
+		return "tom在打 "+name+"游戏!!!!~~~";
+	}
+	
+	@RequestMapping("/getTime")
+	public String getTime(){
+		
+		DateFormat bf = new SimpleDateFormat("yyyy-MM-dd E a HH:mm:ss");//多态
+		
+		return "当前的时间是："+bf.format(new Date());
 	}
 
 }
