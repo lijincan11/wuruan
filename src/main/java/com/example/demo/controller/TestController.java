4239cf34971202d169a9414db14b89510eeb06f2
Xiong Zhao <13657211670@sina.cn>
2018-11-18 15:19:31 +0800

    我提交了  一个新的包  和一个controller

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	
+	@RequestMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name){
+		
+		System.out.println("hello"+name);
+		
+		return "hello"+name;
+	}
+
+}
