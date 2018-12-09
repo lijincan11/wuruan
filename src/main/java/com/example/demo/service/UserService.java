38a5e939dffd9bc8a4d1b780ff68283da41f71d5
lijincan11 <1301220748@qq.com>
2018-12-09 15:03:53 +0800

    注册

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ public class UserService {
 	
 	@Autowired
 	UserDao userDao;
+	Rsp rsp=new Rsp();
 
 	/**
 	 * status  0:success  1:username or password not right  2:username  not exist
@@ -20,7 +23,7 @@ public class UserService {
 	 */
 	public Rsp login(User user) {
 		// TODO Auto-generated method stub
-		Rsp rsp=new Rsp();
+		
 		
 		//num =1  表示 用户名存在
 		int num=userDao.countUserName(user.getUsername());
@@ -43,13 +46,27 @@ public class UserService {
 		
 		return rsp;
 	}
-
+	/**
+	 * status 0: 注册成功        1:username or password is null  2:username or id is exit
+	 * @param user
+	 * @return
+	 */
 	public Rsp register(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		//num =1  表示 用户名和id存在
+			int num=userDao.countNameandId(user.getUsername(), user.getId());
+				
+			if(num==1){
+				rsp.setStatus(1);
+				rsp.setTip("该ID或者用户名已被注册");
+				}else{
+					userDao.insertUser(user);
+					rsp.setStatus(0);
+					rsp.setTip("注册成功");
+			}
+			if(num==2){
+				rsp.setStatus(2);
+				rsp.setTip("注册信息不能为空");
+			}
+			return rsp;
 	}
-
-	
-	
-	
 }
