1812a0635cb0a6f2a1cb2d7f0e44923753b7a392
Xiong Zhao <13657211670@sina.cn>
2018-12-11 15:34:48 +0800

    fix

--- src/test/java/com/example/demo/TestApplicationTests.java
+++ src/test/java/com/example/demo/TestApplicationTests.java
@@ -16,53 +16,13 @@ import com.example.demo.model.User;
 public class TestApplicationTests {
 
 	
-	@Autowired
-	UserDao userDao;
+//	@Autowired
+//	UserDao userDao;
 	
 	@Test
 	public void contextLoads() {
 	}
 	
 	
-	@Test
-	public void test_getUsers(){
-		List<User> userList=userDao.getAllUser();
-		
-		User user;
-		for (int i = 0; i < userList.size(); i++) {
-			user=userList.get(i);
-			System.out.println("用户名："+user.getUsername()+"\t"+"密码："+user.getPassword()+"\t"+"id:"+user.getId());
-		}
-	}
 	
-	
-	@Test
-	public void test_UpdateUser(){
-		User user=new User("marry", "888", "3");
-		
-		int num=userDao.updateUser(user);
-		
-		System.out.println(num==1?"成功":"失败");
-	}
-
-	
-	@Test
-	public void test_deleteuser(){
-		User user=new User("marry", "888", "1");
-		
-		int num=userDao.deleteUser(user);
-		
-		System.out.println(num==1?"成功":"失败");
-	}
-	
-	
-	
-	@Test
-	public void test_adduser(){
-		User user=new User("marry", "888", "1");
-		
-		int num=userDao.insertUser(user);
-		
-		System.out.println(num==1?"成功":"失败");
-	}
 }
